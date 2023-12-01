2c989e751c2d2ca40c555843d5f63bee28546151
JRaams <47593569+JRaams@users.noreply.github.com>
2023-12-01 20:01:37 +0100

    replace 01b with better solution

--- 01/b.ts
+++ 01/b.ts
@@ -1,39 +1,25 @@
 const lines = await Deno.readTextFile("./input.txt");
 const input = lines.split("\n").filter((l) => l);
 
-const stringToNumbers: Record<string, number> = {
-  "1": 1,
-  "2": 2,
-  "3": 3,
-  "4": 4,
-  "5": 5,
-  "6": 6,
-  "7": 7,
-  "8": 8,
-  "9": 9,
-  "one": 1,
-  "two": 2,
-  "three": 3,
-  "four": 4,
-  "five": 5,
-  "six": 6,
-  "seven": 7,
-  "eight": 8,
-  "nine": 9,
-};
-
 function solve(input: string[]): number {
   let sum = 0;
 
   input.forEach((line) => {
-    const first = stringToNumbers[
-      line.match(/\d|one|two|three|four|five|six|seven|eight|nine/)!.at(0)!
-    ];
-    const last = stringToNumbers[
-      line.match(/\d|one|two|three|four|five|six|seven|eight|nine/)!.at(-1)!
-    ];
+    const newLine = line
+      .replaceAll("one", "o1ne")
+      .replaceAll("two", "t2wo")
+      .replaceAll("three", "t3hree")
+      .replaceAll("four", "f4our")
+      .replaceAll("five", "f5ive")
+      .replaceAll("six", "s6ix")
+      .replaceAll("seven", "s7even")
+      .replaceAll("eight", "e8ight")
+      .replaceAll("nine", "n9ine");
 
-    sum += Number(`${first}${stringToNumbers[last]}`);
+    const first = newLine.match(/\d/)!.at(0);
+    const last = newLine.match(/\d/g)!.at(-1);
+
+    sum += Number(`${first}${last}`);
   });
 
   return sum;
