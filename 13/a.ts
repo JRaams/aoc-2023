5f506e9f8a416877c12fc032b5cd9eef834740ff
JRaams <47593569+JRaams@users.noreply.github.com>
2023-12-13 22:06:32 +0100

    cleanup 13a

--- 13/a.ts
+++ 13/a.ts
@@ -4,22 +4,19 @@ const lines = await Deno.readTextFile("./input.txt");
 const patterns: string[] = lines.trim().split("\n\n");
 
 function findMirrorIndex(grid: string[]) {
-  for (let i = 1; i < grid.length; i++) {
+  row: for (let i = 1; i < grid.length; i++) {
     const aboveMirror = grid.slice(0, i).reverse();
     const belowMirror = grid.slice(i);
 
-    let differentChars = 0;
     for (const [row1, row2] of zip(aboveMirror, belowMirror)) {
       for (let x = 0; x < row1.length; x++) {
         if (row1[x] !== row2[x]) {
-          differentChars++;
+          continue row;
         }
       }
     }
 
-    if (differentChars === 0) {
-      return i;
-    }
+    return i;
   }
 
   return 0;
